38e451f0433c9aa26782dd8e1a7d8805dc396704
Itaru7 <ir172178@gmail.com>
2017-06-03 14:34:21 -0700

    myPage

--- user/view/header/header.js
+++ user/view/header/header.js
@@ -12,25 +12,25 @@ var result = "";
 			
 			
 			+"<span class = \"tag\">"
-			+"<span class = \"home\">"
-    			+ "<img src= \"..\/header\/icons\/home.png\">"
-			+ "</span>"
-			
-			+"<span class = \"job\">"
-    		+ "<img src= \"..\/header\/icons\/job.png\">"
-			+ "</span>"
-			
-			+"<span class = \"message\">"
-    		+ "<img src= \"..\/header\/icons\/message.png\">"
-			+ "</span>"
-			
-			+"<span class = \"notification\">"
-    		+ "<img src= \"..\/header\/icons\/notification.png\">"
-			+ "</span>"
-			
-			+"<span class = \"myPage\">"
-    		+ "<img src= \"..\/header\/icons\/myPage.png\">"
-			+ "</span>"
+				+"<span class = \"home\">"
+					+ "<img src= \"..\/header\/icons\/home.png\">"
+				+ "</span>"
+				
+				+"<span class = \"job\">"
+					+ "<img src= \"..\/header\/icons\/job.png\">"
+				+ "</span>"
+				
+				+"<span class = \"message\">"
+					+ "<img src= \"..\/header\/icons\/message.png\">"
+				+ "</span>"
+				
+				+"<span class = \"notification\">"
+					+ "<img src= \"..\/header\/icons\/notification.png\">"
+				+ "</span>"
+				
+				+"<span class = \"myPage\">"
+					+ "<img src= \"..\/header\/icons\/myPage.png\">"
+				+ "</span>"
 			+ "</span>";
 	
 document.write(result);
