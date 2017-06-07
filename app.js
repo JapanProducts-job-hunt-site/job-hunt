dbd79fbde417f79bdb985fd219755713179f936c
Itaru7 <ir172178@gmail.com>
2017-06-07 16:57:47 -0700

    a

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require("express");
+var app = express();
+
+app.use('/static', express.static('public'));
+var port = 4000;
+
+
+app.all("/", function (req, res)
+{
+    console.log(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port ,function()
+{
+    console.log(__dirname);
+
+    console.log("http://localhost:" + port);
+});
