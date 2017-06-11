b2c93b4933f22440bcf0abe4c3d12c868cc0125f
Itaru7 <ir172178@gmail.com>
2017-06-11 13:30:24 -0700

    corrected app.js

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-
 var express = require("express");
 var app = express();
 
-app.use('/static', express.static('/public'));
-var port = 4000;
+/*
+ app.use(express.static(__dirname + '/public'));
+ var port = 3000;
 
 
-app.all("/", function (req, res)
-{
-    // console.log(__dirname + "/index.html");
-    console.log(__dirname + "/index.html");
-    res.sendFile(__dirname + "/index.html");
-});
+ app.all("/", function (req, res)
+ {
+ // console.log(__dirname + "/index.html");
+ console.log(__dirname + "/index.html");
+ res.sendFile(__dirname + "/index.html");
+ });
 
-app.listen(port ,function()
-{
-    console.log(__dirname);
+ app.listen(port ,function()
+ {
+ console.log(__dirname);
 
-    console.log("http://localhost:" + port);
-});
+ console.log("http://localhost:" + port);
+ });
+ */
+
+var port = 3000;
+
+app
+    .all("/", function (req, res) {
+        // console.log(__dirname + "/index.html");
+        console.log(__dirname + "/index.html");
+        res.sendFile(__dirname + "/index.html");
+    })
+    .use(express.static(__dirname + '/public'))
+    .listen(port, function () {
+        console.log(__dirname);
+        console.log("http://localhost:" + port);
+    });
+
+
+/*
+ var port = 3000;
+ app
+ .get('/', function(req, res){
+ res.sendFile(__dirname + "/index.html");
+ })
+ .use(express.static(__dirname + '/public'))
+ .listen(port, function(){
+ console.log(__dirname);
+ console.log("http://localhost:" + port);
+ });
+ */
